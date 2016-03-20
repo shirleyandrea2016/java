b213ea7c9970cb760153090f15103c8510d9d0e0
Daniel Gracia <danielgraciac@gmail.com>
2016-03-20 15:48:01 -0500

    corregido los issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log ("Inicio de programa");
 alert("Hola Javascript");
-document.write("<h1>saludo iunicial</h1>");
-alert ("gracias por visitar")
+document.write("<h1>saludo inicial</h1>");
+alert ("gracias por visitar");
 console.log ("fin de programa");
